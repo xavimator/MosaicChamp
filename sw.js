d95a617d31c0bb32f2a2270426b27b5d505b242c
xavimator <128036794+xavimator@users.noreply.github.com>
2026-07-12 18:43:28 +0200

    New icons

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v29';
+const CACHE_NAME = 'mosaicchamp-v31';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
