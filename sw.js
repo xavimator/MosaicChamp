2c7fd17958b01c3eb28f3525c890708db0553b13
xavimator <128036794+xavimator@users.noreply.github.com>
2026-07-15 21:31:56 +0200

    Sudden test 2

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v35';
+const CACHE_NAME = 'mosaicchamp-v36';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
