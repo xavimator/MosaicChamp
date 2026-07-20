4a098474b9ab4e4f6436a25afc3bb3fc51e27651
xavimator <128036794+xavimator@users.noreply.github.com>
2026-07-20 07:39:54 +0200

    Scribble shape

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v56';
+const CACHE_NAME = 'mosaicchamp-v57';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
