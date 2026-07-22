c6ca967e2ad17caa76504960a16b23818f628621
xavimator <128036794+xavimator@users.noreply.github.com>
2026-07-22 08:33:45 +0200

    New tiles and rules

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v62';
+const CACHE_NAME = 'mosaicchamp-v65';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
