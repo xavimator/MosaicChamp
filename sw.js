c840bcfed31beda1173e12096ccae48f57a514b5
xavimator <128036794+xavimator@users.noreply.github.com>
2026-07-25 21:49:04 +0200

    Finished shadow

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v88';
+const CACHE_NAME = 'mosaicchamp-v89';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
