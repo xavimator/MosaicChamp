50cad436f5e11b1a850b1c8c8c3952eca5efe84f
xavimator <128036794+xavimator@users.noreply.github.com>
2026-07-28 18:58:46 +0200

    User exit fix

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v94';
+const CACHE_NAME = 'mosaicchamp-v95';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
