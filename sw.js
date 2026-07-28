01cca63e634aee5d8b931ebfca32205c337a3854
xavimator <128036794+xavimator@users.noreply.github.com>
2026-07-28 08:34:47 +0200

    Multi level up fix

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v90';
+const CACHE_NAME = 'mosaicchamp-v94';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
