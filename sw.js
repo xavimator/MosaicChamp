3619d29e8179610558416b4bda744408e2c32c21
xavimator <128036794+xavimator@users.noreply.github.com>
2026-07-29 22:51:02 +0200

    Pattern colour fix

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v97';
+const CACHE_NAME = 'mosaicchamp-v98';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
