b42e358493eebc95eaf64ed4745cdcd674297d91
xavimator <128036794+xavimator@users.noreply.github.com>
2026-08-01 23:05:23 +0200

    Pattern table multiplayer sync

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v110';
+const CACHE_NAME = 'mosaicchamp-v111';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
