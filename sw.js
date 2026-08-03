05e1cc48a74cc957967d89a77bdfc7fe79ca1d85
xavimator <128036794+xavimator@users.noreply.github.com>
2026-08-03 20:10:28 +0200

    Otono cosmetics

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v121';
+const CACHE_NAME = 'mosaicchamp-v122';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
