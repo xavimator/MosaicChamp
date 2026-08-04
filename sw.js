3f3b09392fe3f53568a64f6d3fe85247f3f624af
xavimator <128036794+xavimator@users.noreply.github.com>
2026-08-04 20:18:50 +0200

    Shadow leaf v3

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v124';
+const CACHE_NAME = 'mosaicchamp-v126';
 
 // Recursos que se precachean en la instalación
 const PRECACHE = [
