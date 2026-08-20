f6413f5de67dfc73e6613e39181300ee983063ee
xavimator <128036794+xavimator@users.noreply.github.com>
2026-08-20 16:05:48 +0200

    Update CACHE_NAME to version v163

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v162'; // ← v162: Loud secrets cosmetics
+const CACHE_NAME = 'mosaicchamp-v163'; // ← v163: Loud secrets shine
 
 // Recursos que se precachean en la instalación
 // NOTA: rutas relativas (sin "/" inicial) para funcionar tanto si la app
