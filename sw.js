990a8c68238ab7b49d21acc168b2b22c71e3202f
xavimator <128036794+xavimator@users.noreply.github.com>
2026-08-21 10:40:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v168'; // ← v168: Loud secrets hls ctrl
+const CACHE_NAME = 'mosaicchamp-v169'; // ← v169: Loud secrets hls ctrl
 
 // Recursos que se precachean en la instalación
 // NOTA: rutas relativas (sin "/" inicial) para funcionar tanto si la app
