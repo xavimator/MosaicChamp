7ae99f80a60bbb23c79acf78d1070b980ffa07c3
xavimator <128036794+xavimator@users.noreply.github.com>
2026-08-22 23:59:55 +0200

    Update CACHE_NAME to version v174

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v173'; // ← v173: Festividades 2nd try
+const CACHE_NAME = 'mosaicchamp-v174'; // ← v174: Festividades color try
 
 // Recursos que se precachean en la instalación
 // NOTA: rutas relativas (sin "/" inicial) para funcionar tanto si la app
