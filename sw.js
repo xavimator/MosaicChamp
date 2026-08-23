34ab9952791b16ae3bdd1cc488ddc7b3e40aa5e1
xavimator <128036794+xavimator@users.noreply.github.com>
2026-08-23 23:50:25 +0200

    Update CACHE_NAME to version v181

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v180'; // ← v180: new year clock placement 3
+const CACHE_NAME = 'mosaicchamp-v181'; // ← v181: new year NY fireworks restor
 
 
 // Recursos que se precachean en la instalación
