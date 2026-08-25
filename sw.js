4eaa61478b987e1af4e5be1eb20956dd1cedb06d
xavimator <128036794+xavimator@users.noreply.github.com>
2026-08-25 05:45:14 +0200

    NY sliders false

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  El registro se hace automáticamente desde index.html
 // ─────────────────────────────────────────────────────────
 
-const CACHE_NAME = 'mosaicchamp-v189'; // ← v189: NY lobby top 3px + 135% size, sliders quitados
+const CACHE_NAME = 'mosaicchamp-v190'; // ← v190: NY lobby top 3px + 135% size, sliders false
 
 
 // Recursos que se precachean en la instalación
